e2db6751b3e8952bf930c9759d8df1ea39a37e11
MrJ <nxjeff@gmail.com>
2014-07-23 09:34:19 +0800

    Added notification

--- LocationService/src/com/woi/locationservice/MainActivity.java
+++ LocationService/src/com/woi/locationservice/MainActivity.java
@@ -2,9 +2,6 @@ package com.woi.locationservice;
 
 import com.woi.locationservice.services.BgServ;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
--- LocationService/src/com/woi/locationservice/services/BgServ.java
+++ LocationService/src/com/woi/locationservice/services/BgServ.java
@@ -4,12 +4,10 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.woi.locationservice.MainActivity;
